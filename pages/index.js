9b59bd84912ee963d6f2899d7d6d53f495dd47dd
Francisco Alves <fcoocf@hotmail.com>
2021-10-25 18:43:45 +0000

    add componetem react adicionar

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,28 @@
+import { useState } from 'react'
+import { render } from 'react-dom';
+
 function Home(){
-    return <div>Home teste vercel</div>
+    return (
+        <div>
+            <h1>Home teste vercel</h1>
+            <Contador />
+        </div>
+        )
+    
+}
+
+function Contador(){
+    const [contador,setContador] = useState(1)
+
+    function adicionarContador(){
+        setContador(contador + 1);
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
 }
 export default Home
