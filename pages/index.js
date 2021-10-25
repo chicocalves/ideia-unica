7663474c45cfd336d072ef4c32b699bf266a45b8
Francisco Alves <fcoocf@hotmail.com>
2021-10-25 18:53:32 +0000

    teste

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 function Home(){
     return (
         <div>
-            <h1>Home teste vercel</h1>
+            <h1>Home</h1>
             <Contador />
         </div>
         )
