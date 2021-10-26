3f2f5a8b2c6dd7b1f361de6d592862db64da78bb
Francisco Alves <fcoocf@hotmail.com>
2021-10-26 01:52:50 +0000

    teste de getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import { render } from 'react-dom';
+import Link from 'next/link'
 
 function Home(){
     return (
         <div>
             <h1>Home</h1>
+            <Link href="/sobre">Sobre</Link>
             <Contador />
         </div>
         )
