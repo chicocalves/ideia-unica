661bc01fbd01078268e1c3c3f26f317810311185
Francisco Alves <fcoocf@hotmail.com>
2021-10-26 02:53:21 +0000

    add cache na API

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,20 @@
-function tempo(request, response){
+async function tempo(request, response){
+      
+    //const cepApiSecret = process.env.CEP_API_SECRECT
     const dynamicDate = new Date()
 
+    const cepApiSecret = '05077902'
+
+    //const consultaEnderecoResponse = await fetch(`https://viacep.com.br/ws/${cepApiSecret}/json/`);
+    const consultaEnderecoResponse = await fetch(`https://viacep.com.br/ws/${cepApiSecret}/json/`)
+    const consultaEnderecoResponseJson = await consultaEnderecoResponse.json()
+    const endereco = consultaEnderecoResponseJson.logradouro
+
+    response.setHeader('Cache-Control', 's-maxage=10, state-while-revalidate')
+
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        Endereço: endereco
     })
 }
 
